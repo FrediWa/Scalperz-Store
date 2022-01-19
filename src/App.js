7b11e0b634a25e3ad75a658164085e253f72eb32
FrediWa <fredrik.wasstrom@gmail.com>
2022-01-19 11:01:25 +0200

    Add react routes
    * Create folder for components
    
    * Add routes and delete unnecessary files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
+import { BrowserRouter, Link, Routes, Route } from 'react-router-dom'
 import './App.css';
+import Pages from './routes';
+
+const [Cart, Checkout, Products] = Pages
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <nav
+        style={{
+          borderBottom: "solid 1px",
+          paddingBottom: "1rem"
+        }}
+      >
+        <Link to="/products">Products</Link> 
+        <Link to="/cart">Cart</Link>
+      </nav>
+      <Routes>
+        <Route path="products" element={<Products />}/>
+        <Route path="cart" element={<Cart />}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/routes/index.js
@@ -0,0 +1,11 @@
+import Cart from "./Cart"
+import Checkout from './Checkout'
+import Products from './Products'
+
+const Components = [
+    Cart,
+    Checkout,
+    Products
+]
+
+export default Components
