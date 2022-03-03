c54ac7bf6af0d6e36f0a19fbad86e4d98e825653
SikiPriki <70576475+SikiPriki@users.noreply.github.com>
2022-03-03 13:51:46 +0200

    Continuation on checkout (#28)
    * Checkout with API and some cookies
    
    * Getting JWT from Cookie and info from API
    
    * Replaced one cookie in post
    
    Co-authored-by: SikiPriki <halliksi@arcada.fi>

--- src/routes/Checkout.js
+++ src/routes/Checkout.js
@@ -5,34 +5,135 @@ import './styles/Checkout.css'
 import { useState } from 'react';
 import { useCookies } from "react-cookie";
 
+
+const axios = require('axios');
+
 const Checkout = () => {
 
-
+  //Får JWT
+  const getCookieToken = () => {
+    return cookies["user-session"] || null;
+  }
   const [cookies, setCookie] = useCookies(["user"]);
+  const access_token=getCookieToken()
 
-  function handleCookie() {
-    setCookie("item_id", "36D", {
+//Användare
+  const [inputEmail, setInputEmail] = useState('');
+  const [inputAd1, setInputAd1] = useState('');
+  const [userid, setUserid] = useState('');
+  const [useremail, setUseremail] = useState('');
+
+  //Cart saker
+  const [cartProductId, setProductId]=useState('');
+  const [cartProductName, setCartProductName]=useState();
+  const [cartProductAmount, setCartProductAmount]=useState();
+
+  //Produkt saker
+  const [productPrice, setPrice] = useState('');
+ 
+  
+  //Få användar info
+  //https://flaviocopes.com/axios-send-authorization-header/
+  const callUser=()=>{
+      axios.get('https://cna22-user-service.herokuapp.com/users/data', {
+      headers: {
+        'Authorization': `Bearer ${access_token}`
+      }
+      })
+      .then((res) => {
+       
+        setUserid(tokenToJson(access_token).sub)
+        setUseremail(tokenToJson(access_token).email)
+        findAdress(useremail,res.data)
+
+      })
+      .catch((error) => {
+      console.error(error)
+      })
+  }
+  callUser()
+
+//TODO: Få flera saker. 
+  const cartInfoo=()=>{
+    axios.get(`https://cna-cart-api.herokuapp.com/cart/${userid}`,{//använd 2 om din id ej hittas i cart att se att koden funkar${userid}
+      headers: {
+        'Authorization': `Bearer ${access_token}` 
+      }
+      })
+      .then((result) => { 
+
+        setProductId(result.data[0].pId)
+        console.log(result.data[0].pId)
+        setCartProductName(result.data[0].productName)
+        setCartProductAmount(result.data[0].productAmount)
+        console.log(result.data[0].productAmount)
+
+      })
+      .catch((error) => {
+      console.error(error)
+      })
+    }
+    
+  cartInfoo()
+
+
+//Hantera token
+  const tokenToJson= (token) =>{
+    var base64Url = token.split('.')[1];
+    var base64 = base64Url.replace('-', '+').replace('_', '/');
+    return JSON.parse(atob(base64));
+  }
+  
+//Hitta användar address
+  const findAdress=(mail,data)=>{
+      for(var i=0;i<data.length;i++){
+        if(mail===data[i].email){
+          console.log(data[i].email)
+          setInputEmail(data[i].email)
+        setInputAd1(data[i].adress+' '+data[i].zip)
+        }
+      }
+  }
+
+
+//Cookie för om man behöver testa något eller sakerna vid test skedje ej finns
+ const handleCookie=()=> {
+    setCookie("item_id", "394739127971", {
       path: "/"
     });
-    setCookie("productAmount", "1", {
-      path: "/"
-    });
-    setCookie("user_id", "15", {
+    setCookie("productAmount", "3", {
       path: "/"
     });
   }
 
+  //Räkna ut priset
+  const getPrice=()=>{
+    axios.get(`https://cna22-products-service.herokuapp.com/product/${cartProductId}`)// Ändra till cookies.item_id om något ej funkar att se att man faktist får info t.e.x när cart ej har saken i sig
+      .then((res)=>{
+        setPrice(res.data.price*cartProductAmount)//
+      })
+    }
+    getPrice()
+    
+    console.log(cartProductId)
 
-  const [inputEmail, setInputEmail] = useState('');
-  const [inputAd1, setInputAd1] = useState('');
-
-  
-  const purchaseForm= useRef(null)
-
-  //Div byte
-  const continu=()=>{
-      const confirmForm = document.getElementById('confirmationDiv');
-      const inputForm = document.getElementById('purchaseDiv')
+  //Hanterar submit
+  const handleSubmit=(event)=>{
+    event.preventDefault();
+    axios.post("https://quiet-meadow-01451.herokuapp.com/orders",{
+      
+                  customerNumber: userid,
+                  email:inputEmail,
+                  itemId: cartProductId,
+                  address: inputAd1,
+                  price: productPrice
+    },{headers: {
+      'Authorization': `Bearer ${access_token}` 
+    }})
+   
+    //Confirmation info
+    const confirmForm = document.getElementById('confirmationDiv');
+    const inputForm = document.getElementById('purchaseDiv')
 
       if (confirmForm.style.display === 'none') {
           confirmForm.style.display = 'block';
@@ -43,25 +144,8 @@ const Checkout = () => {
          inputForm.style.display = 'block';
       }
   }
-
-
-  function handleSubmit(event) {
-    event.preventDefault();
-
-    let formData = purchaseForm.current;
-  
-    let data = JSON.stringify([{"userId": cookies.user_id,"itemId": cookies.item_id, 
-      "Address": formData['address1'].value}]);
-
-    data = JSON.parse(data);
-    data.forEach(function(element){
-        console.log(element);
-    });
-
-    alert("Object greated in Console")
-  }
-
  
+
   return (
       <div >
           <div onLoad={handleCookie()}>
@@ -70,33 +154,28 @@ const Checkout = () => {
      
                 <p>Delivery information</p>
                 <p>If this is the first time being on this site, please reload to get the cookie information</p>
-
-                <form ref={purchaseForm}>
-                    <h3>Payers Email</h3>
-                    <input type={'email'} name={'email'} onInput={e => setInputEmail(e.target.value)} ></input>
-
-                    <h3>Delivery Address</h3>
-                    <textarea type={'text'} rows={'7'} cols={'20'} name={'address1'} onInput={e =>  setInputAd1(e.target.value)}></textarea>
-                </form>
-
-                <br></br>
-                <button className='confirmationButton' onClick={continu}>Confirm purchase</button>
-              </div>
-          
-
-              <div id={'confirmationDiv'} >
+                <p>Product: {cartProductName}</p>
                 <form name={'confirmationForm'} onSubmit={handleSubmit}>
                     <h3>Email</h3>   
                     <input value={inputEmail} readOnly  />
                     <h3>Address</h3>
-                    <textarea  rows={'7'} cols={'20'} value={inputAd1} readOnly />
+                    <textarea  rows={'7'} cols={'20'}  value={inputAd1} readOnly />
                
                     <br></br>
                     <input className='confirmationButton' type={'submit'} value={'Submit'}></input>
                  </form>
+                <p>Price: {productPrice}</p>
+                <br></br>
+             
+              </div>
+          
 
-                 <button className='goBackButton' onClick={continu}>Go back</button>
-
+              <div id={'confirmationDiv'} >
+                <p>The purchase was sugsessful!</p>
+                <p>We has sent the invoice to: {inputEmail} </p>
+                <p>Delivered to: {inputAd1}</p>
+                <p>Products: {cartProductName} </p>
+                <p>Price: {productPrice}</p>
               </div>  
           </div>
       </div> 
